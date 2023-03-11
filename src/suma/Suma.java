4ba67fe47bda2ac42e074bf67e21403042371079
Iamfrans <1323544@senati.pe>
2023-03-11 10:48:07 -0500

    Entrada de datos

---
+++ src/suma/Suma.java
@@ -0,0 +1,16 @@
+//impotamos el paqute java.util,Scanner para permitir la entrada de datos 
+package suma;
+import java.util.Scanner;
+        
+/**
+ *
+ * @author Alumno
+ */
+public class Suma {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}

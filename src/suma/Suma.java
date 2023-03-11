49387a5294136784142972c926a2a699a61797b1
Iamfrans <1323544@senati.pe>
2023-03-11 11:05:40 -0500

    Prueba 1.0

--- src/suma/Suma.java
+++ src/suma/Suma.java
@@ -15,6 +15,19 @@ public class Suma {
        int num1;
        int num2;
        int suma; 
+       //Uilizams el metodo Sysem.out.println para imprimir caenas de texto
+       
+       System.out.println("Ingresar un numero: ");
+       
+       num1 = obj.nextInt();
+       
+       System.out.println("Ingresar el segundo numero: ");
+       
+       num2 = obj.nextInt();
+       
+       Sysem.out,println("La suma es: "+suma);
+               
+       
     
     }
     
